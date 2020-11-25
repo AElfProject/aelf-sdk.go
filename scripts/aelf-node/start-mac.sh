7cf676eae5b5ccdcee32fae76486c699345b2960
zhxymh <sean.zhang@aelf.io>
2020-11-25 17:57:12 +0800

    update test node and proto files

--- scripts/aelf-node/start-mac.sh
+++ scripts/aelf-node/start-mac.sh
@@ -3,7 +3,7 @@ brew services start redis
 brew install unzip
 mkdir -p ~/.local/share/aelf/keys
 cd ../../
-wget https://github.com/AElfProject/AElf/releases/download/v1.0.0-preview1/aelf.zip && unzip aelf.zip
+wget https://github.com/AElfProject/AElf/releases/download/v1.0.0-rc1/aelf.zip && unzip aelf.zip
 cp scripts/aelf-node/keys/SD6BXDrKT2syNd1WehtPyRo3dPBiXqfGUj8UJym7YP9W9RynM.json  ~/.local/share/aelf/keys/
 cp scripts/aelf-node/app* aelf/
 echo "start node"
