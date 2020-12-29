d8def83e7158cfb026bd6346581d014ec72545e1
zhxymh <sean.zhang@aelf.io>
2020-12-29 20:56:08 +0800

    fix bug

--- dto/block_header_dto.go
+++ dto/block_header_dto.go
@@ -6,13 +6,14 @@ import (
 
 //BlockHeaderDto BlockHeaderDto
 type BlockHeaderDto struct {
-	PreviousBlockHash            string
-	MerkleTreeRootOfTransactions string
-	MerkleTreeRootOfWorldState   string
-	Extra                        string
-	Height                       int64
-	Time                         time.Time
-	ChainId                      string
-	Bloom                        string
-	SignerPubkey                 string
+	PreviousBlockHash                string
+	MerkleTreeRootOfTransactions     string
+	MerkleTreeRootOfWorldState       string
+	MerkleTreeRootOfTransactionState string
+	Extra                            string
+	Height                           int64
+	Time                             time.Time
+	ChainId                          string
+	Bloom                            string
+	SignerPubkey                     string
 }
